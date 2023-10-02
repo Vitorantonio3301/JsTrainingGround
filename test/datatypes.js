6f3c0432c90a76fb6da8bcb389a4dd62dae47ddf
Vitor Antonio <Vitorantonion3301@gmail.com>
2023-10-02 02:55:15 -0300

    Better code on datatypes.js

---
+++ test/datatypes.js
@@ -0,0 +1,15 @@
+//Data types//
+const myName = "Vitor"; //String type
+const string = "Hello World"; //String type
+const age = 20; //Number type
+const javaScriptIsAwesome = true; //Boolean type
+const emptyVariable = null; //Null type
+const undefinedVarbiales = undefined; //Undefined type
+let undefinedVarbiales2; //Undefined type
+
+function myself (){
+    const hello = `My name is ${myName} and i am ${age} year old`; 
+
+    console.log(hello);
+    console.log(typeof age); 
+}
