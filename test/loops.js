a6e48a70831ace496f7205370a91acb49ffabad7
Vitor Antonio <Vitorantonion3301@gmail.com>
2023-10-03 00:59:02 -0300

    Improving the code and testing loops

--- test/loops.js
+++ test/loops.js
@@ -1,42 +1,44 @@
-const objectArray = [
+const userList = [
+    
+        { id: 1, name: 'Vitor', age: '20', country: 'Brazil', Language: 'Portuguese', },
+        
+        { id: 2, name: 'Ozi', age: '44', country: 'France', Language: 'French', },
+        
+        { id: 3, name: 'Marcus', age: '25', country: 'EUA', Language: 'English', },
 
-    {
-        languages:{
-            id: 1,
-            portuguese: 'Native',
-            french: 'Begginer',
-            spanish: 'Advanced',
-            english: 'Fluent',
-        },
-
-    },
-
-    {
-        cars:{
-            id: 2,
-            Toyota: 'Family car',
-            Honda: 'Sports car',
-            bmw: 'Branding car',
-            tesla: 'Future car',
-        },
-
-    },
+        { id: 4, name: 'Sonny', age: '31', country: 'Italy', Language: 'Italian', },
+        
+        { id: 5, name: 'Vinci', age: '18', country: 'India', Language: 'Hindu', },
 
 ];
 
-for( let i = 0; i <= objectArray.length; i++ ){
+function forLoop (){
+    
+    for( let i = 0; i <= userList.length; i++ ){
 
-    console.log( `The object for this array is: ${objectArray[i]}` );
+    console.log( `The id for this array is: ${userList[i].id}` );
+    
+    };
+};
+
+function whileLoop(){
+
+    let i = 0;
+    
+    while( i < userList.length ){
+    
+        console.log( `The object for this array is: ${userList[i].id}` );
+        
+    };
 
 };
 
-let i = 0;
 
-while( i < objectArray.length ){
+function callTest (){
 
-    console.log( `The object for this array is: ${objectArray[i]}` );
-    
-}
+    console.log( forLoop() );
+    console.log( whileLoop() );
+};
 
-
+callTest();
 //console.log( personObject(), objectArray[0].languages);
